ee17b5734810751515c5cfd92cf7bfd5e03a480d
olbapgit <57667079+olbapgit@users.noreply.github.com>
2021-02-21 20:10:01 +0100

    Estilo Nav
    Se ordena como debe

--- src/components/batoinav.vue
+++ src/components/batoinav.vue
@@ -3,33 +3,35 @@
     <a class="navbar-brand" href="/"
       ><img src="@/assets/favicon.png" height="35" width="35"
     /></a>
-    <div class="nav-item">
-          <a class="nav-link" href="/productos">Productos</a>
+    <div class="navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/productos">Productos</a> 
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">Acerca de</a>
+        </li>
+      </ul>
     </div>
-    
-    <div class="nav-item">
-          <a class="nav-link" href="/about">Acerca de</a>      
-    </div>
-
-    <div class="float-right nav-item">
+    <div class="nav-item btn-right">
       <a class="nav-link btn btn-blue" href="/perfil" v-if="user"
         ><b-icon-person-fill></b-icon-person-fill
       ></a>
     </div>
 
-    <div class="float-right nav-item">
+    <div class="nav-item btn-right">
       <a class="nav-link btn-blue" href="/carrito" v-if="user"
         ><b-icon-cart-fill></b-icon-cart-fill
       ></a>
     </div>
 
-    <div class="float-right nav-item">
+    <div class="nav-item btn-right">
       <a class="nav-link btn btn-red" href="/logout" v-if="user"
         ><b-icon-box-arrow-left></b-icon-box-arrow-left>Logout</a
       >
     </div>
 
-    <div class="float-right nav-item">
+    <div class="nav-item btn-right">
       <a class="nav-link btn btn-green" href="/login"
         ><b-icon-box-arrow-right></b-icon-box-arrow-right>Login</a
       >
